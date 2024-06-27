c45729721d7cdf5dee33346da249748c664212ae
Ganesh S Kudva <ganeshskudva@gmail.com>
2024-06-26 18:02:26 -0700

    Create L1650.py

---
+++ Medium/L1650.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        a, b = p, q
+        while a != b:
+            a = q if not a else a.parent
+            b = p if not b else b.parent
+        
+        return a
