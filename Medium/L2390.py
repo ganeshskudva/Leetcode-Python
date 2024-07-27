dc37bd4b4866ecae2af9933e21976feb5f0b5a7d
Ganesh S Kudva <ganeshskudva@gmail.com>
2024-07-27 14:32:38 -0700

    Create L2390.py

---
+++ Medium/L2390.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        st = []
+
+        for c in s:
+            if c != '*':
+                st.append(c)
+            elif st:
+                st.pop()
+
+        return ''.join(st)
+        
