4eea048f4b376bab49540632d73fd5088caf9b50
Ganesh S Kudva <ganeshskudva@gmail.com>
2024-07-31 07:32:05 -0700

    Update L993.py

--- Easy/L993.py
+++ Easy/L993.py
@@ -4,6 +4,8 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# BFS
 class Solution:
     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
         q = deque()
@@ -29,3 +31,20 @@ class Solution:
                     q.append((lvl, node.val, node.right))
 
         return x_parent != y_parent and x_lvl == y_lvl
+
+
+# DFS
+class Solution:
+    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
+        res = []
+
+        def solve(node, parent=None, depth=0):
+            if not node:
+                return
+            if node.val == x or node.val == y:
+                res.append((node.val, parent, depth))
+            solve(node.left, node, depth + 1)
+            solve(node.right, node, depth + 1)
+
+        solve(root)
+        return res[0][1] != res[1][1] and res[0][2] == res[1][2]
