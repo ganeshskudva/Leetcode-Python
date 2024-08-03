5c39a20b1db88fd57de0de0fe050c3efafaf29ee
Ganesh S Kudva <ganeshskudva@gmail.com>
2024-08-03 15:02:15 -0700

    Update L639.py

--- Hard/L639.py
+++ Hard/L639.py
@@ -42,4 +42,32 @@ class Solution:
             return memo[i]
 
         return dfs(0)
+
+
+# Another solution
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        memo = {}
+        MOD = 1000000007
+
+        one = {'1': 1, '2': 1, '3': 1, '4': 1, '5': 1, '6': 1, '7': 1, '8': 1, '9': 1, '*': 9}
+        two = {'10': 1, '11': 1, '12': 1, '13': 1, '14': 1, '15': 1, '16': 1, '17': 1, '18': 1, '19': 1, '20': 1, '21': 1,
+            '22': 1, '23': 1, '24': 1, '25': 1, '26': 1, '*0': 2, '*1': 2, '*2': 2, '*3': 2, '*4': 2, '*5': 2, '*6': 2,
+            '*7': 1, '*8': 1, '*9': 1, '1*': 9, '2*': 6, '**': 15}
+
+        def dfs(i):
+            if i in memo:
+                return memo[i]
+            if i == len(s):
+                return 1
+            
+            res = one.get(s[i:i+1], 0) * dfs(i + 1)
+            if i + 1 < len(s):
+                res += two.get(s[i:i+2], 0) * dfs(i + 2)
+            
+            memo[i] = res % MOD
+            return memo[i]
+
+        return dfs(0)
+        
         
