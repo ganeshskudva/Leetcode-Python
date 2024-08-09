95272e57f6c5bbf505c30a1767e6323004d72e85
Ganesh S Kudva <ganeshskudva@gmail.com>
2024-08-09 16:51:52 -0700

    Create L714.py

---
+++ Medium/L714.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        mp = {}
+
+        def solve(idx, buy=True):
+            if idx >= len(prices):
+                return 0
+
+            key = (idx, buy)
+            if key in mp:
+                return mp[key]
+
+            if buy:
+                mp[key] = max(-prices[idx] + solve(idx + 1, not buy), solve(idx + 1, buy))
+            else:
+                mp[key] = max(prices[idx] - fee + solve(idx + 1, not buy), solve(idx + 1, buy))
+            return mp[key]
+
+        return solve(0)
