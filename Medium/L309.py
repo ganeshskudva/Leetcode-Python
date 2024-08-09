866fa5b956d99ac82cd6f3fac4e2194bc0f5f37b
Ganesh S Kudva <ganeshskudva@gmail.com>
2024-08-09 16:49:25 -0700

    Create L309.py

---
+++ Medium/L309.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        mp = {}
+
+        def solve(idx, buy=True):
+            if idx >= len(prices):
+                return 0
+            key = (idx, buy)
+            if key in mp:
+                return mp[key]
+            
+            if buy:
+                mp[key] = max(-prices[idx] + solve(idx + 1, not buy), solve(idx + 1, buy))
+                return mp[key]
+            
+            mp[key] = max(prices[idx] + solve(idx + 2, not buy), solve(idx + 1, buy))
+            return mp[key]
+        
+        return solve(0)
