dc77be814ef70e52029c2128cd941b41984d9ded
Ganesh S Kudva <ganeshskudva@gmail.com>
2024-08-09 16:13:47 -0700

    Create L123.py

---
+++ Hard/L123.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        mp = {}
+
+        def solve(idx, k, buy=True):
+            if idx >= len(prices) or k <= 0:
+                return 0
+
+            key = (idx, buy, k)
+            if key in mp:
+                return mp[key]
+
+            if buy:
+                mp[key] = max(-prices[idx] + solve(idx + 1, k, not buy), solve(idx + 1, k, buy))
+            else:
+                mp[key] = max(prices[idx] + solve(idx + 1, k - 1, not buy), solve(idx + 1, k, buy))
+
+            return mp[key]
+
+        return solve(0, 2)
