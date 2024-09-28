9bbb707c54d47bd1aa116e9798b5f56fe35c44be
Ganesh S Kudva <ganeshskudva@gmail.com>
2024-09-28 07:21:41 -0700

    Update with comments
    Update with comments

--- Medium/L1650.py
+++ Medium/L1650.py
@@ -10,9 +10,16 @@ class Node:
 
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        # Initialize two pointers, 'a' starting at node p and 'b' starting at node q
         a, b = p, q
+        
+        # Traverse upwards until both pointers are equal
         while a != b:
+            # Move pointer 'a' to its parent, or reset to q if 'a' reaches the root (None)
             a = q if not a else a.parent
+            
+            # Move pointer 'b' to its parent, or reset to p if 'b' reaches the root (None)
             b = p if not b else b.parent
         
+        # Both pointers will eventually meet at the lowest common ancestor, return it
         return a
