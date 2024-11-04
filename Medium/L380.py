2a52840e07ec5319a4066fba6f1b801ce10c7568
Ganesh S Kudva <ganeshskudva@gmail.com>
2024-11-04 10:09:08 -0800

    Simplify
    Simplify

--- Medium/L380.py
+++ Medium/L380.py
@@ -1,11 +1,10 @@
 import random
-from collections import defaultdict
 
 class RandomizedSet:
 
     def __init__(self):
         # Dictionary to store element index mappings
-        self.mp = defaultdict(int)
+        self.mp = {}
         # List to store the elements
         self.nums = []
 
@@ -13,7 +12,7 @@ class RandomizedSet:
         # Check if val is already present
         if val in self.mp:
             return False
-        # Append val to the list and update its index in the dictionary
+        # Append val to the list and store its index in the dictionary
         self.nums.append(val)
         self.mp[val] = len(self.nums) - 1
         return True
@@ -22,25 +21,25 @@ class RandomizedSet:
         # Check if val is present in the set
         if val not in self.mp:
             return False
-        # Replace the element to be removed with the last element
-        if self.mp[val] < len(self.nums) - 1:
-            last_val = self.nums[-1]
-            self.nums[self.mp[val]] = last_val
-            self.mp[last_val] = self.mp[val]
-        # Remove the last element from the list
+        # Get the last element in the list and the index of the element to be removed
+        last_val = self.nums[-1]
+        idx_to_remove = self.mp[val]
+        # Move the last element to the index of the element to be removed
+        self.nums[idx_to_remove] = last_val
+        self.mp[last_val] = idx_to_remove
+        # Remove the last element from the list and the val from the dictionary
         self.nums.pop()
-        # Remove val from the dictionary
         del self.mp[val]
         return True
 
     def getRandom(self) -> int:
         # Return a random element from the list
-        return self.nums[random.randint(0, len(self.nums) - 1)]
+        return random.choice(self.nums)
 
-# Time Complexity (TC):
-# - insert: O(1), as we add to the end of the list and update the dictionary.
-# - remove: O(1), as we swap and remove the last element from the list and update the dictionary.
-# - getRandom: O(1), as we access a random element by index in the list.
+# Time Complexity:
+# - insert: O(1)
+# - remove: O(1)
+# - getRandom: O(1)
 
-# Space Complexity (SC):
-# - O(N), where N is the number of elements in the set, as both the dictionary and list store up to N elements.
+# Space Complexity:
+# - O(N), where N is the number of elements in the set
