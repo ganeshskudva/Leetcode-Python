be54203a45fb6ec1431fcb55d45d5a1d3abff399
Ganesh S Kudva <ganeshskudva@gmail.com>
2024-11-12 04:50:02 -0800

    Update comments
    Update comments

--- Hard/L224.py
+++ Hard/L224.py
@@ -12,24 +12,25 @@ class Solution:
                 if op == "/":
                     stack.append(int(stack.pop() / v))  # Perform integer division on the last value and the current number.
 
-            # Initialize variables: num for current number, stack to hold values, sign for current operator.
+            # Initialize variables: num for the current number, stack to hold values, sign for current operator.
             num, stack, sign = 0, [], "+"
-            
+
             # Iterate through the string starting from index 'it'
             while it < len(s):
                 if s[it].isdigit():                    # If the current character is a digit.
                     num = num * 10 + int(s[it])        # Build the current number (account for multi-digit numbers).
                 elif s[it] in "+-*/":                  # If an operator is encountered.
-                    update(sign, num)                 # Update the stack with the current number based on the previous sign.
-                    num, sign = 0, s[it]              # Reset the number and update the sign to the current operator.
-                elif s[it] == "(":                    # If an opening parenthesis is encountered.
-                    num, it = calc(it + 1)            # Recursively call calc for the inner expression.
-                elif s[it] == ")":                    # If a closing parenthesis is encountered.
-                    update(sign, num)                 # Process the current number before closing the parentheses.
-                    return sum(stack), it + 1         # Return the sum of the current stack and the position after the closing parenthesis.
+                    update(sign, num)                  # Update the stack with the current number based on the previous sign.
+                    num, sign = 0, s[it]               # Reset the number and update the sign to the current operator.
+                elif s[it] == "(":                     # If an opening parenthesis is encountered.
+                    num, tmp = calc(it + 1)            # Recursively call calc for the inner expression.
+                    it = tmp - 1                       # Adjust 'it' to the position after the nested expression.
+                elif s[it] == ")":                     # If a closing parenthesis is encountered.
+                    update(sign, num)                  # Process the current number before closing the parentheses.
+                    return sum(stack), it + 1          # Return the sum of the current stack and the position after the closing parenthesis.
                 it += 1
-            update(sign, num)                         # Process the last number after the loop finishes.
-            return sum(stack)
+            update(sign, num)                          # Process the last number after the loop finishes.
+            return sum(stack)                          # Return the sum of the stack for the outermost expression.
 
         # Begin processing the expression starting at index 0.
         return calc(0)
